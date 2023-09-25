e6cffe185d6f371bc174350908a0483b118c6e59
Luckyshan <luckyshanb4@gmail.com>
2023-09-25 15:14:54 +0530

    first commit

---
+++ src/main/java/com/lucky/circlecispringbootdemo/CircleciSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.circlecispringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleciSpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucky/circlecispringbootdemo/CircleciSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.circlecispringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleciSpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
